100cbeee01ff052ea12a04f1d872ef1c08c256d3
mastik8 <mastik8@gmail.com>
2024-04-13 13:07:29 +0300

    added logo

--- src/components/FitterNavbar.js
+++ src/components/FitterNavbar.js
@@ -1,16 +1,10 @@
 import React from 'react';
-import { Navbar, Nav, Form, Row, Col, Button } from 'react-bootstrap'
+import { Navbar, Nav, Form, Row, Col, Button, Image } from 'react-bootstrap'
 const FitterNavbar = () => {
     return (
         <Navbar className="bg-body-tertiary justify-content-between" bg="dark" data-bs-theme="dark">
             <Navbar.Brand href="/">
-                <img
-                    src="/path/to/fitter-logo.png"
-                    width="30"
-                    height="30"
-                    className="d-inline-block align-top"
-                    alt="Fitter"
-                />
+                <Image src="icons/fitter-logo.jpg" width="100" height="50" className="d-inline-block align-top" alt="Fitter" rounded />
             </Navbar.Brand>
             <Form inline>
                 <Row>
