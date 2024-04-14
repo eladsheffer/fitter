cdd47ae02db09d280d66c589a46a61dcece92a26
Din Eilie (דין אילי) <98718983+DinEilie@users.noreply.github.com>
2024-04-14 18:54:12 +0300

    Update  - 14.04.2024
    ( - ) Homepage layout with groups and events.
    ( - ) Adding JSON files.
    ( - ) Adding new routes.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
-import { Route, Routes, BrowserRouter, HashRouter } from 'react-router-dom';
+import "./App.css";
+import { Route, Routes, BrowserRouter, HashRouter } from "react-router-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Homepage from './pages/Homepage';
-import GroupsPage from './pages/GroupsPage';
-import EventsPage from './pages/EventsPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Homepage from "./pages/Homepage";
+import GroupsPage from "./pages/GroupsPage";
+import EventsPage from "./pages/EventsPage";
+import GroupPage from "./pages/GroupPage";
+import EventPage from "./pages/EventPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
   const router = createBrowserRouter([
     {
       path: "/",
@@ -18,13 +19,19 @@ function App() {
       element: <GroupsPage />,
     },
     {
+      path: "/groups/:id",
+      element: <GroupPage />,
+    },
+    {
       path: "/events",
       element: <EventsPage />,
     },
-  ])
-  return (
-    <RouterProvider router={router} />
-  );
+    {
+      path: "/events/:id",
+      element: <EventPage />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
