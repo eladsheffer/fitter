943c6d302fbce98e3cd0c1ab2e3cf1607cf6dc3e
mastik8 <mastik8@gmail.com>
2024-09-11 14:36:36 +0300

    made the navbar fixed to top

--- src/components/FitterNavbar.js
+++ src/components/FitterNavbar.js
@@ -112,7 +112,7 @@ function FitterNavbar() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="fixed">
       <Container maxWidth="xl">
         <Toolbar disableGutters>
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
