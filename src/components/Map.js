1d87ce5aba001b099324944b28ea6f16ee87d080
mastik8 <mastik8@gmail.com>
2024-09-13 12:15:51 +0300

    switched embedded map to google maps

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,6 +10,7 @@ const OpenStreetMapIframeWithAddress = ({ address }) => {
       const url = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(address)}&format=json`;
       
         const data = await getData(url);
+        console.log("data:", data);
         if (data && data.length > 0) {
           const { lat, lon } = data[0];
           // Parse lat/lon as floats and calculate a smaller bbox margin
@@ -18,7 +19,8 @@ const OpenStreetMapIframeWithAddress = ({ address }) => {
           const bbox = `${lonFloat - 0.004},${latFloat - 0.004},${
             lonFloat + 0.004
           },${latFloat + 0.004}`;
-          const src = `https://www.openstreetmap.org/export/embed.html?bbox=${bbox}&layer=mapnik&marker=${lat},${lon}&zoom=14`; // Added zoom parameter
+          //const src = `https://www.openstreetmap.org/export/embed.html?bbox=${bbox}&layer=mapnik&marker=${lat},${lon}&zoom=14`; // Added zoom parameter
+          const src = `https://maps.google.com/maps?q=${lat},${lon}&hl=he&t=&z=16&ie=UTF8&iwloc=B&output=embed`
           console.log("src:", src);
           setIframeSrc(src);
         } else {
@@ -32,16 +34,21 @@ const OpenStreetMapIframeWithAddress = ({ address }) => {
   return (
     <div>
       <h1>Map for: {address}</h1>
-      {iframeSrc ? (
+      {iframeSrc ? (<>
         <iframe
           width="100%"
-          height="500"
+          height="450"
           src={iframeSrc}
           style={{ border: "1px solid black" }}
           allowFullScreen=""
-          loading="lazy"
+          //loading="lazy"
           title="OpenStreetMap Embed"
+          frameborder="0"
+          marginheight="0" 
+          marginwidth="0" 
+          id="gmap_canvas"
         ></iframe>
+      </>
       ) : (
         <LinearProgress />
       )}
